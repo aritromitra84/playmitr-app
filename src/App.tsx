b5c0f0a67d0d5480f753dfd2705a0c0fc58d1882
Aritro Mitra <aritro.mitra84email@gmail.com>
2025-12-03 20:32:39 +0530

    test husky

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="p-8 bg-yellow-100 min-h-screen">
+      <h1 className="text-4xl font-extrabold text-blue-600">
+        Tailwind is working!
+      </h1>
+
+      <p className="mt-4 text-xl">
+        Phase 1 is complete. Ready for Phase 2.
+      </p>
+    </div>
+  );
+}
